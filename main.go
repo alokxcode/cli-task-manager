6022e04649cc12894a87207c196839eecec10343
Alok <alokxyz.07@gmail.com>
2025-12-02 06:38:17 +0000

    saving todo in json file- feature added

--- main.go
+++ main.go
@@ -2,18 +2,39 @@ package main
 
 import (
 	// "bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 )
 
-var TodoList = make([]Todo, 0)
+var TodoList []Todo
 
 type Todo struct {
-	sNo  uint
-	task string
+	Id   uint   `json:"id"`
+	Task string `json:"task"`
 }
 
 func main() {
+	_, err := os.Lstat("todos.json")
+	var IsfileExist bool
+	if err != nil {
+		IsfileExist = false
+	} else {
+		IsfileExist = true
+	}
+
+	if IsfileExist {
+		data, err := os.ReadFile("todos.json")
+		if err != nil {
+			fmt.Println("Error while Reading todos.json")
+		}
+		err = json.Unmarshal(data, &TodoList)
+		if err != nil {
+			fmt.Println("Error while unmarshalling todos.json", err)
+		}
+		fmt.Println(TodoList)
+	}
+
 	var newTodo string
 	command := os.Args
 
@@ -23,22 +44,24 @@ func main() {
 		for _, word = range command[2:] {
 			newTodo = fmt.Sprintf("%v %v", newTodo, word)
 		}
+
 		noOfTodos := len(TodoList)
-		var todo = Todo{
-			sNo:  uint(noOfTodos) + 1,
-			task: newTodo,
+		NewTodo := Todo{
+			Id:   uint(noOfTodos) + 1,
+			Task: newTodo,
 		}
-		TodoList = append(TodoList, todo)
-		var todos Todo
-		for _, todos = range TodoList {
-			fmt.Printf("[%v] -> %v\n", todos.sNo, todos.task)
+
+		TodoList = append(TodoList, NewTodo)
+		data, _ := json.Marshal(TodoList)
+		err = os.WriteFile("todos.json", data, 0644)
+		if err != nil {
+			fmt.Println("Error while saving todo", err)
+		} else {
+			fmt.Println(newTodo, "saved")
 		}
 
 	case "tdlist":
-		var todo Todo
-		for _, todo = range TodoList {
-			fmt.Printf("%v -> %v", todo.sNo, todo.task)
-		}
+
 	}
 
 	// reader := bufio.NewReader(os.Stdin)
